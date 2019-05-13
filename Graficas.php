28478b5f3212ab04a3405879e7ff5daf08521502
Kronos <mauricio_noble6@hotmail.com>
2019-05-12 19:25:06 -0500

    diseno y estructura de Graficas.php

--- Graficas.php
+++ Graficas.php
@@ -29,59 +29,126 @@
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/navbar_css.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/login_css.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Roboto" rel="stylesheet">
     <meta charset="utf-8">
-    <title>Agenda Web</title>
+    <title>Agenda Web Graficas</title>
     <!--<link rel="stylesheet" href="assets/css/style.css">-->
   </head>
 <body>     
 
   <?php require 'partials/headerAdmin.php' ?>
+  <div class="container">
+      <h1 class="rale">Graficas</h1>
+    <div class="row justify-content-md-center">
+      <div class="man col-md-10">
+        <div class="row">
+          <div class="col-md-6 linea">
+            <h1 class="titw">Horas de Uso</h1>
+            <br>
+            <form action="graficaHorasUso">
+              <div class="row noms">
+                <div class="col-xs ancho">
+                  <p style="color: #ffffff; text-align:center;">Inicio</p>
+                  <input required="true" type="time" name="inicioHorasUso" id="inicioHorasUso" class="form-control" oninput="validarHora()">
+                </div>
+                
+                <div  class="col-xs ancho">
+                  <p style="color: #ffffff; text-align:center;">Fin</p>
+                  <input required="true" type="time" name="finHorasUso" id="finHorasUso"  class="form-control">
+                </div>
+              </div>
+              <br>
+              <input type="submit" name="Insertar" value="Enviar" class="form-control bot">
+            </form>
+          </div>
+          <div class="col-md-6">
+          <h1 class="titw">Horas por día de la semana</h1>
+
+          <br>
+            <form action="graficaDiaSemana">
+              <div class="row noms">
+                <div class="col-xs ancho">
+                  <p style="color: #ffffff; text-align:center;">Inicio</p>
+                  <input required="true" type="time" name="inicioSemana" id="inicioSemana" class="form-control" oninput="validarHora2()">
+                </div>
+                <div  class="col-xs ancho">
+                  <p style="color: #ffffff; text-align:center;">Fin</p>
+                  <input required="true" type="time" name="finSemana" id="finSemana"  class="form-control">
+                </div>
+              </div>
+              <br>
+              <input type="submit" name="Insertar" value="Enviar" class="form-control bot">
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <style>
+    .titw{
+      color: white;
+      font-size:150%;
+      text-align:center;
+    }
+    @media (max-width: 767px) {
+      .linea {
+        border-bottom: 2px solid white;
+        margin-bottom: 10px;
+        padding-bottom: 10px;
+      }
+    }
+    @media (min-width: 768px) {
+      .linea {
+        border-right: 2px solid white;
+      }
+    }
+
+    .ancho{
+      width:40%;
+    }
+    .noms{
+      display:flex;
+      justify-content: space-around;
+    }
+  </style>
+  <script>
+      function validarHora() {
+          var horaInicio = document.getElementById("inicioHorasUso").value;
+          var remplazoUltimoNum = (parseInt(horaInicio.charAt(horaInicio.length - 1)) + 1) + "";
+
+          if (remplazoUltimoNum == '10') {
+              remplazoUltimoNum = '9';
+          }
+          var horaMinima = horaInicio.replaceAt((horaInicio.length - 1), remplazoUltimoNum);
+          console.log(horaMinima);
+
+          document.getElementById("finHorasUso").min = horaMinima;
+      }
+      function validarHora2() {
+          var horaInicio = document.getElementById("inicioSemana").value;
+          var remplazoUltimoNum = (parseInt(horaInicio.charAt(horaInicio.length - 1)) + 1) + "";
+
+          if (remplazoUltimoNum == '10') {
+              remplazoUltimoNum = '9';
+          }
+          var horaMinima = horaInicio.replaceAt((horaInicio.length - 1), remplazoUltimoNum);
+          console.log(horaMinima);
+
+          document.getElementById("finSemana").min = horaMinima;
+      }
+      String.prototype.replaceAt = function(index, replacement) {
+          return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+      }
+  </script>
 
 
 <?php 
     if (!empty($User)):?>
     <div class="Holis"> 
-    <canvas id="myChart"></canvas>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>    
-    
-    <script>  
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var chart = new Chart(ctx, {
-    // The type of chart we want to create
-        type: 'bar',
-
-        <?php
-              require 'dabase.php';
-              $sentencia= "SELECT * FROM Laboratorios WHERE Visibilidad = 1";
-              $Ejecutar = sqlsrv_query($con,$sentencia);
-              $labs = "";
-
-
-                while ($resultados = sqlsrv_fetch_array($Ejecutar,SQLSRV_FETCH_ASSOC)) {
-                  //echo $resultados['Laboratorio'].",";
-                  $labs=$labs."'".$resultados['Laboratorio']."',";
-                };
-                echo $labs;
-             ?>
-    // The data for our dataset
-        data: {
-            labels: [],
-            datasets: [{
-                label: 'Usos por laboratorio',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [2, 1, 1, 2, 0, 5,]
-            }]
-        },
-
-    // Configuration options go here
-    options: {}
-});</script>
-
-<p>Nota: esta seccion sigue en desarrollo</p>
 
   <?php else: ?>
     <h1>Please Login or SignUp</h1>
@@ -89,23 +156,7 @@
     <a href="signup.php">SignUp</a>
   <?php endif;?>
 
-    <style type="text/css">
-    body{
-      background-color: #f9f7f7;
-    }
-    .bie{
-      font-family: "Raleway", sans-serif;
-      text-align: center;
-      font-size: 3rem;
-      font-weight: lighter;
-      margin-top: 28vh;
-      color: #3f72af;
-    }
-    .Holis{
-    display: flex;
-    justify-content: space-around;
-    }
-  </style>
+    
 
 </body>
 </html>
