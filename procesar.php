c8c97b70912c0a1e12077c6869bdb62a71cb8695
Kronos <mauricio_noble6@hotmail.com>
2019-05-14 10:43:44 -0500

    Carga desde csv terminada

--- procesar.php
+++ procesar.php
@@ -16,16 +16,19 @@
 		$fp = fopen ("$carpeta$excel","r");
 
 		//fgetcsv. obtiene los valores que estan en el csv y los extrae.
-
 		while ($data = fgetcsv ($fp, 1000, ","))
 		{
 			//si la linea es igual a 1 no guardamos por que serian los titulos de la hoja del excel.
 			if ($row!=1)
 			{
-
 				$num = count($data);
-				$insertar="INSERT INTO Reserva (title,Grupo,UnidadAprendizaje,Asunto,color,Laboratorios,Usuario,textColor,start,fin) VALUES ('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8]','$data[9]')";
-				$sql = sqlsrv_query ($con,$insertar) or die(print_r(sqlsrv_errors(),true));
+				$insertar="INSERT INTO Reserva (title,Grupo,UnidadAprendizaje,Asunto,color,Laboratorios,Usuario,textColor,start,fin) VALUES ('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]',convert(datetime,'$data[8]',103),convert(datetime,'$data[9]',103))";
+				$sql = sqlsrv_query ($con,$insertar) or die(print_r('
+				<script>
+					alert(`Carga completada`);
+					document.location.replace(\'Importar.php\');
+				</script>
+				',true));
 				if (!$sql){
 					echo "<div>Hubo un problema al momento de importar porfavor vuelva a intentarlo</div >";
 					exit;
